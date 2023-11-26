eaffb76433305139edc1da5f31f4ee1d1c10f5af
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-26 07:42:31 +0000

    Add files via upload

---
+++ range.2.py
@@ -0,0 +1,20 @@
+
+for x in range (1,11):
+     print(x)
+     #this dispplay from 1 to 10
+   
+for y in range(11):
+     print("y",y)  #this is display from 0 to 10
+for z in range(2,11):
+     print("z",z)  #this is display 2 to 11
+for w in range(2,20,2):
+    print("w",w) # this is display 2,4,6,8,10 to 20
+for q in range(ord("A") ,ord("Z")+1):
+     print("q",q)
+     print("char",chr(q))
+
+
+
+
+
+
